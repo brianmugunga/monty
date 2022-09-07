8a06378edf2466abaa986be99a6b296f08cdba48
brianmugunga <104896959+brianmugunga@users.noreply.github.com>
2022-09-07 13:59:04 +0300

    Update and rename errors_2.c to free_lineptr.c

---
+++ free_lineptr.c
@@ -0,0 +1,22 @@
+/*
+ * File: free_lineptr.c
+ * Auth: Brian Mugunga
+ */
+
+#include "monty.h"
+
+/**
+ * free_lineptr - free line pointer returned by getline
+ * @status: exit status
+ * @arg: pointer to line
+ *
+ * Return: void
+ */
+void free_lineptr(int status, void *arg)
+{
+	char **lineptr = arg;
+
+	(void)status;
+	if (*lineptr != NULL)
+		free(*lineptr);
+}
