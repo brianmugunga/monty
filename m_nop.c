f0b70c5beca08142439139410be76f171d10b954
brianmugunga <104896959+brianmugunga@users.noreply.github.com>
2022-09-07 14:05:28 +0300

    Update and rename set_op_toks_error.c to m_nop.c

---
+++ m_nop.c
@@ -0,0 +1,19 @@
+/*
+ * File: m_nop.c
+ * Auth: BRIAN MUGUNGA
+ */
+
+#include "monty.h"
+
+/**
+ * m_nop - no operation performed
+ * @stack: double pointer to head of stack
+ * @line_number: line number of current operation
+ *
+ * Return: void
+ */
+void m_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
